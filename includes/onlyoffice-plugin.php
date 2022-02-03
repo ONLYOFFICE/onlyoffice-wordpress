3c0d1fd6e1c9c0f90da35223daf084f9c496f5ad
Andrew <andrew.panov@onlyoffice.com>
2022-02-03 11:29:27 +0300

    plugin loader

--- includes/onlyoffice-plugin.php
+++ includes/onlyoffice-plugin.php
@@ -3,6 +3,7 @@
 class OOPlugin
 {
 
+    protected $loader;
     protected $plugin_name;
     protected $version;
 
@@ -11,9 +12,35 @@ class OOPlugin
 
         $this->version = ONLYOFFICE_PLUGIN_VERSION;
         $this->plugin_name = 'onlyoffice-plugin';
+
+        $this->load_dependencies();
+    }
+
+    private function load_dependencies()
+    {
+
+        require_once plugin_dir_path(dirname(__FILE__)) . 'includes/onlyoffice-loader.php';
+        $this->loader = new OOP_Loader();
+    }
     }
 
     public function run()
     {
+        $this->loader->run();
+    }
+
+    public function get_plugin_name()
+    {
+        return $this->plugin_name;
+    }
+
+    public function get_loader()
+    {
+        return $this->loader;
+    }
+
+    public function get_version()
+    {
+        return $this->version;
     }
 }
