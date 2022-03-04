8b7e63098f67f2e0459a96a1c356fc1a8b71842b
Oleg Sinizin <oleg.sinizin@onlyoffice.com>
2022-03-04 12:25:56 +0300

    check mobile device

--- public/views/editor.php
+++ public/views/editor.php
@@ -172,6 +172,11 @@ class OOP_Editor
                     config.width = "100%";
                     config.height = "100%";
 
+                    if (/android|avantgo|playbook|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od|ad)|iris|kindle|lge |maemo|midp|mmp|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\\|plucker|pocket|psp|symbian|treo|up\\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i
+                        .test(navigator.userAgent)) {
+                        config.type='mobile';
+                    }
+
                     docEditor = new DocsAPI.DocEditor("iframeEditor", config);
                 };
 
