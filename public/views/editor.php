e912280382d00792ed92f038a00e7ccbaa4b0b8d
Oleg Sinizin <oleg.sinizin@onlyoffice.com>
2022-05-24 22:48:50 +0300

    add $user as param for wp_login; update activate/deactivate functions

--- public/views/editor.php
+++ public/views/editor.php
@@ -290,7 +290,7 @@ class Onlyoffice_Plugin_Editor
             if ($user_id !== null && $user) {
                 wp_set_current_user($user_id, $user->user_login);
                 wp_set_auth_cookie($user_id);
-                do_action('wp_login', $user->user_login);
+                do_action('wp_login', $user->user_login, $user);
             } else {
                 wp_die("No user information", '', array('response' => 403));
             }
@@ -370,7 +370,7 @@ class Onlyoffice_Plugin_Editor
         if ($user_id !== null && $user) {
             wp_set_current_user( $user_id, $user->user_login );
             wp_set_auth_cookie( $user_id );
-            do_action( 'wp_login', $user->user_login );
+            do_action( 'wp_login', $user->user_login, $user);
         } else {
             wp_die("No user information", '', array('response' => 403));
         }
