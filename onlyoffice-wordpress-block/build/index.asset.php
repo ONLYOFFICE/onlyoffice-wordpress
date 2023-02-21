9ab6e693ea0ea11492dce81bd58ec985ad5192c4
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2023-02-21 15:05:54 +0300

    update: onlyoffice-wordpress-block

--- onlyoffice-wordpress-block/build/index.asset.php
+++ onlyoffice-wordpress-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-polyfill'), 'version' => '27ac65bb1dee09b27736');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-polyfill'), 'version' => '89c793e5697816a6a807');
