3d9a1ee7ad656dd0147b278fa995b52492b29cb2
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2024-04-03 16:40:15 +0300

    build: bump deps for onlyoffice-wordpress-block

--- onlyoffice-wordpress-block/build/index.asset.php
+++ onlyoffice-wordpress-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '6b0bf00445ec7a1d5aa1');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '4f7c984e58f9263b6891');
